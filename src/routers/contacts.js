cea9ce754da662f3b5a3ecb5161ed348534fa927
Kyrylko <kkuzmenko1992@gmail.com>
2024-10-26 12:15:44 -0230

    comleated step 2(validation)

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,16 +8,36 @@ import {
   deleteContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema } from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValid.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactsByIdController),
+);
 
-router.post('/contacts', ctrlWrapper(createContactsControllers));
+router.post(
+  '/',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactsControllers),
+);
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch(
+  '/contacts/:contactId',
+  isValidId,
+  validateBody(createContactSchema),
+  ctrlWrapper(patchContactController),
+);
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
 export default router;
---
+++ src/validation/contacts.js
@@ -0,0 +1,14 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username shuld have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().min(10).max(15).required(),
+  email: Joi.string().email().required(),
+  isFavorite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+});
