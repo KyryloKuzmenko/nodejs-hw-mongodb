0397127204e26507e3b75c09294dfb1d529b517f
Kyrylko <kkuzmenko1992@gmail.com>
2024-10-28 14:20:43 -0230

    fixed stuck(added next in validateBody)

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -5,6 +5,7 @@ export const validateBody = (schema) => async (req, res, next) => {
         await schema.validateAsync(req.body, {
             abortEarly: false,
         });
+        next();
     } catch (error) {
         const err = createHttpError(400, 'Bad Request', {
             errors: error.details,
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,13 +4,13 @@ export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required().messages({
     'string.base': 'Username should be a string',
     'string.min': 'Username should have at least {#limit} characters',
-    'string.max': 'Username shuld have at most {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
     'any.required': 'Username is required',
   }),
   phoneNumber: Joi.string().min(6).max(16).required(),
   email: Joi.string().email().required(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid("work", "home", "personal").required(),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
 export const updateContactSchema = Joi.object({
@@ -18,5 +18,5 @@ export const updateContactSchema = Joi.object({
   phoneNumber: Joi.string().min(6).max(16),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid("work", "home", "personal"),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
