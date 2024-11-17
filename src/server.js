4ce19a930868ba6dcc14bff58f398aa75ec4d73a
Kyrylko <kkuzmenko1992@gmail.com>
2024-11-17 01:02:09 -0330

    comleted 3-8 steps

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = env('PORT', '3000');
 
@@ -30,10 +31,14 @@ export const setupServer = () => {
     }),
   );
   app.use(router);
+
+  app.use('/api-docs', swaggerDocs());
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
-  app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
